388aee6e588f3abda9f94f19c8281d71fd7d91d7
Stephen Van Gordon <stephen.van.gordon@gmail.com>
2016-11-27 17:13:52 -0700

    try to include terrain type

--- src/utilities/mapConstructor.js
+++ src/utilities/mapConstructor.js
@@ -15,20 +15,38 @@ export const mapConstructor = (mapJSON) => {
 	// TODO: Consider moving this whole thing to syncMap, and running it everytime we sync
 	const constructedMap = []
   // add tiles
+	const firstgid = mapJSON.tilesets[0].firstgid // this will break when we add more tilesets
 	for (let i = 0; i < mapJSON.height * mapJSON.width; i++) {
 		const tile = {
+			id: i,
 			layers: {},
 			y: Math.floor(i / mapJSON.width),
 			x: i % mapJSON.width
 		}
-		tile.id = i
+		// loop over all layers and add
 		for (let k = 0; k < mapJSON.layers.length; k++) {
 			// const layerObj = tile.layers
+			const layer = mapJSON.layers[k]
+			// const layerName = layer.name
+			const tileData = layer.data[i] - firstgid
+			let terrainTypeIndex
+			try {
+				terrainTypeIndex = tileData !== -1 ? mapJSON.tilesets[0].tiles[tileData]
+					.terrain.find(terrainType => terrainType !== -1)
+				: null
+			} catch (e) {
+				terrainTypeIndex = null
+			}
+			// const terrainTypeIndex = tileData !== -1 ? mapJSON.tilesets[0].tiles[tileData]
+			// 	.terrain.find(terrainType => terrainType !== -1)
+			// : null
+			const terrainType = terrainTypeIndex ? mapJSON.tilesets[0].terrains[terrainTypeIndex] : null
 			tile.layers[mapJSON.layers[k].name] = {
-				data: mapJSON.layers[k].data[i] - mapJSON.tilesets[0].firstgid,
+				data: mapJSON.layers[k].data[i] - firstgid,
         name: mapJSON.layers[k].name,
-				offset: mapJSON.layers[k].data[i] - mapJSON.tilesets[0].firstgid !== -1 ?	tileGetter(mapJSON.layers[k].data[i] - 1) : null,
-				opacity: 1
+				offset: mapJSON.layers[k].data[i] - firstgid !== -1 ?	tileGetter(mapJSON.layers[k].data[i] - 1) : null,
+				opacity: 1,
+				terrainType
 			}
 		}
 		// if (i === 82) {
