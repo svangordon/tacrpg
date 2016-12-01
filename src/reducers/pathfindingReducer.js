4cb9497371c2c01d9adbc15145fdfc4d8b649670
Stephen Van Gordon <stephen.van.gordon@gmail.com>
2016-12-01 13:47:14 -0700

    writen a way to generate movemaps for units

--- src/reducers/pathfindingReducer.js
+++ src/reducers/pathfindingReducer.js
@@ -12,20 +12,36 @@ function pathfinding(state = initialState, action) {
   switch (action.type) {
     case PATHFINDING_ACTIONS.GET_MOVE_MAPS: {
       // console.log('getting move Maps', action)
+      // console.log(JSON.stringify(action))
       const terrainmap = action.terrainmap
+      const width = 15 // Its a problem that I'm hardcoding this. idk where to put it?
+      const len = terrainmap.length
+      const getNeighbors = node => {
+        /*
+          . 0 .
+          1 * 2
+          . 3 .
+        */
+        let neighbors = []
+        neighbors[0] = node - width >= 0 ? node - width : null
+        neighbors[1] = node % width > 0 ? node - 1 : null
+        neighbors[2] = (node + 1) % width !== 0 ? node + 1 : null
+        neighbors[3] = node + width < len ? node + width : null
+        return neighbors
+      }
       const movemaps = action.units.map(unit => {
         // generate a move map for the unit
         // const getMoveCost = node =>
         const getNodeCost = node => { // move cost of entering a given node
-          console.log(terrainmap[node].layers.land.terrainType.properties.moveCost)
           const moveCost = JSON.parse(terrainmap[node].layers.land.terrainType.properties.moveCost)[unit.moveType]
           console.log(moveCost)
         }
         getNodeCost(112)
         const movemap = []
         const startNode = unit.position
-
       })
+
+      // const getNeighborsCost
 
       return Object.assign({}, state, {
         turnBegun: true
